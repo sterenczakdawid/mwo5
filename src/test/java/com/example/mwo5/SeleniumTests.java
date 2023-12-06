c71117b4e5b78571e52cb741b985d141b37926bb
Dawid <sterenczak.dawid@gmail.com>
2023-12-06 17:35:19 +0100

    changes

--- src/test/java/com/example/mwo5/SeleniumTests.java
+++ src/test/java/com/example/mwo5/SeleniumTests.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -33,7 +34,10 @@ public class SeleniumTests {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless"); // Dodaj opcję headless
+
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @AfterEach
